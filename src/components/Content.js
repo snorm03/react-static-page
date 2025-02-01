4381d4a19077bd10dcf0756ecc5a041ece17fdb7
Sarah Norman <sarahnorman@Sarahs-MacBook-Pro.local>
2025-02-01 11:26:51 -0500

    update2

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,9 +8,9 @@ function Content() {
             <p>
             The Three Little Pigs is a fairy tale about three pig siblings
             who each build a house. The first pig builds of straw, the 
-            second a house of sticks, and third a hous of bricks.
-            A big bad wolf huffs and puffs and blows down the straw house. It then
-            huffs and puffs and blows down the second house. The wolf, however, fails to
+            second a house of sticks, and third a house of bricks.
+            A big bad wolf huffs and puffs and blows the straw house down. It then
+            makes its way to the stick house and blows it down as well. The wolf, however, fails to
             destroy the sturdy brick house. The pigs in the brick house outsmart the
             wolf, who ultimately gives up.
             </p>
